df4a8860b369e26f49adef6e6d4e1e69dcb45f60
ahmedBassell <ahmedbassell@gmail.com>
2016-12-17 15:55:00 +0200

    init docker & node server & docker-compose

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+var mongoose = require('mongoose');
+
+//DB setup
+mongoose.connect('mongodb://mongo:27017');
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
